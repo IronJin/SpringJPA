9953658f2bcea976382e54efd108397b348220b4
IronJin <85132068+IronJin@users.noreply.github.com>
2022-08-01 00:23:08 +0900

    MemberDomain

---
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,37 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor //final 이 붙어있는 경우에 생성자를 만들어줌
+public class MemberRepository {
+
+    private final EntityManager em;
+
+    //회원가입
+    public Long save(Member member) {
+        em.persist(member);
+        return member.getId();
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        return em.createQuery("select m from Member m", Member.class).getResultList();
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+}
